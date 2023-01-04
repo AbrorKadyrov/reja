b51a0c90b3548b9d2caf56748b6f9a77709e721f
vito98 <abror.qodirov.97@gmail.com>
2023-01-04 16:49:53 +0900

    Portfolio publishing

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 Kirish code
 
@@ -22,6 +32,10 @@ app.set("view engine", "ejs");
 app.post("/create-item", function (req, res) {
   console.log(req.body);
   res.json({ test: "success" });
+});
+
+app.get("/develop", (req, res) => {
+  res.render("develop", { user: user });
 });
 
 app.get("/", function (req, res) {
